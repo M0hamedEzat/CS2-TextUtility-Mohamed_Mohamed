063e5bd29373f42c61c3d27d66c1ebadae25acb2
M0hamedEzat <ezatm1083@gmail.com>
2025-06-24 14:13:56 +0300

    Added main.cpp

--- text_utility.h
+++
@@ -1,8 +0,0 @@
-int wordCount (string);
-int charCount (string);
-int vowelConstantCount (string);
-string reversal (string);
-bool palindrome (string);
-string toUpperCase (string);
-string toLowerCase (string);
-int substringCount (string, string);
