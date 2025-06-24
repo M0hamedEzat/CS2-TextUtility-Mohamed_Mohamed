263dd0be84952863d980d0a4a9b90d6335bf9f6e
M0hamedEzat <ezatm1083@gmail.com>
2025-06-24 14:53:36 +0300

    added the cmake and build files

---
+++ mybuild/CMakeFiles/trial.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trial.dir/main.cpp.o"
+  "CMakeFiles/trial.dir/main.cpp.o.d"
+  "CMakeFiles/trial.dir/text_utility.cpp.o"
+  "CMakeFiles/trial.dir/text_utility.cpp.o.d"
+  "libtrial.dll.a"
+  "trial.exe"
+  "trial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mybuild/CMakeFiles/trial.dir/text_utility.cpp.o.d
@@ -0,0 +1,19 @@
+CMakeFiles/trial.dir/text_utility.cpp.o: \
+ C:/msys64/home/ezatm/MAKE/text_utility.cpp \
+ C:/msys64/home/ezatm/MAKE/text_utility.h \
+ C:/msys64/ucrt64/include/c++/15.1.0/cstring \
+ C:/msys64/ucrt64/include/c++/15.1.0/bits/version.h \
+ C:/msys64/ucrt64/include/c++/15.1.0/x86_64-w64-mingw32/bits/c++config.h \
+ C:/msys64/ucrt64/include/c++/15.1.0/x86_64-w64-mingw32/bits/os_defines.h \
+ C:/msys64/ucrt64/include/c++/15.1.0/x86_64-w64-mingw32/bits/cpu_defines.h \
+ C:/msys64/ucrt64/include/c++/15.1.0/pstl/pstl_config.h \
+ C:/msys64/ucrt64/include/string.h C:/msys64/ucrt64/include/crtdefs.h \
+ C:/msys64/ucrt64/include/corecrt.h C:/msys64/ucrt64/include/_mingw.h \
+ C:/msys64/ucrt64/include/_mingw_mac.h \
+ C:/msys64/ucrt64/include/_mingw_secapi.h \
+ C:/msys64/ucrt64/include/vadefs.h \
+ C:/msys64/ucrt64/include/sdks/_mingw_ddk.h \
+ C:/msys64/ucrt64/include/sec_api/string_s.h \
+ C:/msys64/ucrt64/include/c++/15.1.0/cctype \
+ C:/msys64/ucrt64/include/ctype.h \
+ C:/msys64/ucrt64/include/corecrt_wctype.h
